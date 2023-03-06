a29dd60031933bc6b548a004057f186c2dfb224b
Nathan Lee <nathan@globalphobia.com>
2023-03-06 15:38:25 -0700

    disable noice (needs some work. hijacked rubocop and compiler messages and was unable to respond to dialog questions)

--- home/nvim/lua/plugins/ui.lua
+++ home/nvim/lua/plugins/ui.lua
@@ -51,68 +51,69 @@ return {
   },
 
   -- noicer ui
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
-    opts = {
-      lsp = {
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-      },
-      presets = {
-        bottom_search = false,
-        -- command_palette = true,
-        long_message_to_split = true,
-        inc_rename = true,
-        -- cmdline_output_to_split = true,
-      },
-      views = {
-        cmdline_popup = {
-          position = {
-            row = 50,
-            col = "50%",
-          },
-          size = {
-            min_width = 60,
-            width = "auto",
-            height = "auto"
-          },
-        },
-        popupmenu = {
-          relative = "editor",
-          position = {
-            row = 53,
-            col = "50%",
-          },
-          size = {
-            width = 60,
-            height = "auto",
-            max_height = 15,
-          },
-          border = {
-            style = "rounded",
-            padding = { 0, 1 },
-          },
-          win_options = {
-            winhighlight = { Normal = "Normal", FloatBorder = "NoiceCmdlinePopupBorder" },
-          },
-        },
-      },
-    },
-    -- stylua: ignore
-    keys = {
-      { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
-      { "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
-      { "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
-      { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
-      { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll forward" },
-      { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward"},
-    },
-  },
+  -- 2023-01-24 disable for now. Noice is hijacking messages that need input and there is no way to respond. IE Metals
+  -- {
+  --   "folke/noice.nvim",
+  --   event = "VeryLazy",
+  --   dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
+  --   opts = {
+  --     lsp = {
+  --       override = {
+  --         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+  --         ["vim.lsp.util.stylize_markdown"] = true,
+  --         ["cmp.entry.get_documentation"] = true,
+  --       },
+  --     },
+  --     presets = {
+  --       bottom_search = false,
+  --       -- command_palette = true,
+  --       long_message_to_split = true,
+  --       inc_rename = true,
+  --       -- cmdline_output_to_split = true,
+  --     },
+  --     views = {
+  --       cmdline_popup = {
+  --         position = {
+  --           row = 50,
+  --           col = "50%",
+  --         },
+  --         size = {
+  --           min_width = 60,
+  --           width = "auto",
+  --           height = "auto"
+  --         },
+  --       },
+  --       popupmenu = {
+  --         relative = "editor",
+  --         position = {
+  --           row = 53,
+  --           col = "50%",
+  --         },
+  --         size = {
+  --           width = 60,
+  --           height = "auto",
+  --           max_height = 15,
+  --         },
+  --         border = {
+  --           style = "rounded",
+  --           padding = { 0, 1 },
+  --         },
+  --         win_options = {
+  --           winhighlight = { Normal = "Normal", FloatBorder = "NoiceCmdlinePopupBorder" },
+  --         },
+  --       },
+  --     },
+  --   },
+  --   -- stylua: ignore
+  --   keys = {
+  --     { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
+  --     { "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
+  --     { "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
+  --     { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
+  --     { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll forward" },
+  --     { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward"},
+  --   },
+  -- },
 
     -- dashboard
   {
