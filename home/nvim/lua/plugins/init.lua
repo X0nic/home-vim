1642c98aad8798b39154e1b2af8b2401cf86088d
Nathan Lee <nathan@globalphobia.com>
2023-05-19 09:59:05 -0600

    add surround support

--- home/nvim/lua/plugins/init.lua
+++ home/nvim/lua/plugins/init.lua
@@ -243,11 +243,22 @@ return {
     },
   },
 
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
+
   { "tpope/vim-fugitive" },
   -- { "Lokaltog/vim-easymotion" },
   -- use "rstacruz/sparkup", {'rtp': 'vim/'}
 
-  { "tpope/vim-surround" },
+  -- { "tpope/vim-surround" },
   { "tpope/vim-rails",
     dependencies = {"tpope/vim-projectionist"}
   },
