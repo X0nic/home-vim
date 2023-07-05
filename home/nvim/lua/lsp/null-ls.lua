1f19437383023faea92c591010f6516615dbdaa4
Nathan Lee <nathan@globalphobia.com>
2023-07-04 22:53:29 -0600

    update

--- home/nvim/lua/lsp/null-ls.lua
+++ home/nvim/lua/lsp/null-ls.lua
@@ -1,4 +1,70 @@
 local null_ls = require("null-ls")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+local lsp_formatting = function(buffer)
+  vim.lsp.buf.format({
+    filter = function(client)
+      -- By default, ignore any formatters provider by other LSPs
+      -- (such as those managed via lspconfig or mason)
+      -- Also "eslint as a formatter" doesn't work :(
+      return client.name == "null-ls"
+    end,
+    bufnr = buffer,
+  })
+end
+
+local async_formatting = function(bufnr)
+  bufnr = bufnr or vim.api.nvim_get_current_buf()
+
+  vim.lsp.buf_request(
+    bufnr,
+    "textDocument/formatting",
+    vim.lsp.util.make_formatting_params({}),
+    function(err, res, ctx)
+      if err then
+        local err_msg = type(err) == "string" and err or err.message
+        -- you can modify the log message / level (or ignore it completely)
+        vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
+        return
+      end
+
+      -- don't apply results if buffer is unloaded or has been modified
+      if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
+        return
+      end
+
+      if res then
+        local client = vim.lsp.get_client_by_id(ctx.client_id)
+        vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
+        vim.api.nvim_buf_call(bufnr, function()
+          vim.cmd("silent noautocmd update")
+        end)
+      end
+    end
+  )
+end
+
+-- Format on save
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Avoiding-LSP-formatting-conflicts#neovim-08
+local on_attach = function(client, buffer)
+  -- the Buffer will be null in buffers like nvim-tree or new unsaved files
+  if (not buffer) then
+    return
+  end
+
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = buffer })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = buffer,
+      callback = function()
+        -- lsp_formatting(buffer)
+        async_formatting(buffer)
+        -- vim.lsp.buf.formatting_sync()
+      end,
+    })
+  end
+end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
@@ -8,10 +74,14 @@ local diagnostics = null_ls.builtins.diagnostics
 -- https://github.com/prettier-solidity/prettier-plugin-solidity
 null_ls.setup {
   debug = false,
+  on_attach = on_attach,
   sources = {
+    -- formatting.prettier.with {
+    --   extra_filetypes = { "toml" },
+    --   extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+    -- },
     formatting.prettier.with {
       extra_filetypes = { "toml" },
-      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
     },
     formatting.black.with { extra_args = { "--fast" } },
     formatting.stylua,
