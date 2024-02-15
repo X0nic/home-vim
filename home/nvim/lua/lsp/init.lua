369459a905bc45e736414207644367d7714a7bca
Nathan Lee <nathan@globalphobia.com>
2024-02-15 12:25:17 -0700

    fix lsp

--- home/nvim/lua/lsp/init.lua
+++ home/nvim/lua/lsp/init.lua
@@ -1,52 +1,132 @@
-local lsp = require('lsp-zero')
-lsp.preset('recommended')
+local lsp_zero = require('lsp-zero')
 
-lsp.configure('solargraph', {
-  settings = {
-    solargraph = {
-      diagnostics = false,
-    },
-  },
-  init_options = {
-    formatting = false
-  }
-})
+lsp_zero.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
 
-lsp.configure('lua_ls', {
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { 'vim' }
-      },
-    },
-  },
-})
-
-lsp.configure("yamlls", {
-  settings = {
-    yaml = {
-      keyOrdering = false
-    }
-  }
-})
-
-local navic = require("nvim-navic")
-lsp.on_attach(function(client, bufnr)
-  if client.server_capabilities.documentSymbolProvider then
-    navic.attach(client, bufnr)
-  end
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
-lsp.setup()
-
-vim.diagnostic.config({
-  virtual_text = true,
-  signs = true,
-  update_in_insert = false,
-  underline = true,
-  severity_sort = false,
-  float = true,
+-- to learn how to use mason.nvim with lsp-zero
+-- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {'tsserver', 'rust_analyzer', 'solargraph'},
+  handlers = {
+    lsp_zero.default_setup,
+    lua_ls = function()
+      local lua_opts = lsp_zero.nvim_lua_ls()
+      require('lspconfig').lua_ls.setup(lua_opts)
+    end,
+  }
 })
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+
+-- this is the function that loads the extra snippets to luasnip
+-- from rafamadriz/friendly-snippets
+require('luasnip.loaders.from_vscode').lazy_load()
+
+cmp.setup({
+  sources = {
+    {name = 'copilot'},
+    {name = 'path'},
+    {name = 'nvim_lsp'},
+    {name = 'nvim_lua'},
+    {name = 'luasnip', keyword_length = 2},
+    {name = 'buffer', keyword_length = 3},
+  },
+  formatting = lsp_zero.cmp_format(),
+  mapping = cmp.mapping.preset.insert({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm({
+      -- documentation says this is important.
+      -- I don't know why.
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = false,
+    })
+  }),
+})
+
+-- local lsp = require('lsp-zero')
+-- lsp.preset('recommended')
+--
+-- lsp.configure('solargraph', {
+--   settings = {
+--     solargraph = {
+--       diagnostics = false,
+--     },
+--   },
+--   init_options = {
+--     formatting = false
+--   }
+-- })
+--
+-- lsp.configure('lua_ls', {
+--   settings = {
+--     Lua = {
+--       diagnostics = {
+--         globals = { 'vim' }
+--       },
+--     },
+--   },
+-- })
+--
+-- lsp.configure("yamlls", {
+--   settings = {
+--     yaml = {
+--       keyOrdering = false
+--     }
+--   }
+-- })
+--
+-- local navic = require("nvim-navic")
+-- lsp.on_attach(function(client, bufnr)
+--   if client.server_capabilities.documentSymbolProvider then
+--     navic.attach(client, bufnr)
+--   end
+-- end)
+--
+-- lsp.setup()
+--
+-- vim.diagnostic.config({
+--   virtual_text = true,
+--   signs = true,
+--   update_in_insert = false,
+--   underline = true,
+--   severity_sort = false,
+--   float = true,
+-- })
+
+-- local cmp = require('cmp')
+-- local cmp_action = lsp.cmp_action()
+--
+-- cmp.setup({
+--   sources = {
+--     {name = 'copilot'},
+--     {name = 'nvim_lsp'},
+--   },
+--   mapping = cmp.mapping.preset.insert({
+--     ['<CR>'] = cmp.mapping.confirm({
+--       -- documentation says this is important.
+--       -- I don't know why.
+--       behavior = cmp.ConfirmBehavior.Replace,
+--       select = false,
+--     })
+--   })
+-- })
 
 -- require("lsp.ruby")
 require "lsp.null-ls"
